f1342a1050ec74fac082ff03065c8a30384bef04
Samuel Sampaio <samuel.sampaio@conectrj.com.br>
2024-10-22 09:24:14 -0300

    fix: retorna 0 quando não houver jobs rodando e sempre retorna 0 mesmo se não estiver entre 9h e 11h para zerar a trigger caso ela ative

--- bacula-running-zabbix-sender.bash
+++ bacula-running-zabbix-sender.bash
@@ -9,14 +9,27 @@ output=$(echo "status dir" | bconsole)
 # Conta o número de jobs que estão rodando (status Running)
 running_jobs=$(echo "$output" | grep -c "is running")
 
+# Se não houver nenhum job rodando, define o valor como 0
+if [[ -z "$running_jobs" ]]; then
+    running_jobs=0
+fi
+
 # Captura o horário atual (hora em formato de 24h)
 current_hour=$(date +"%-H")
 
 # Sempre envia o número de jobs rodando para o Zabbix
-$zabbixSender -z $zabbixServer -c $zabbixAgentConfig -s "$baculaHost" -k "bacula.running.jobs" -o "$running_jobs"
+$zabbixSender -z $zabbixServer \
+        -c $zabbixAgentConfig \
+        -s "$baculaHost" \
+        -k "bacula.running.jobs" \
+        -o "$running_jobs"
 
-# Verifica se o horário está entre 9h e 13h (inclusive) e se há mais de 1 job rodando
-if [[ $current_hour -ge 9 && $current_hour -le 11 && $running_jobs -ge 1 ]]; then
+# Verifica se o horário está entre 9h e 11h ou se o número de jobs rodando for 0
+if [[ ($current_hour -ge 9 && $current_hour -le 11 && $running_jobs -ge 1) || $running_jobs -eq 0 ]]; then
     # Envia o alerta para o Zabbix
-    $zabbixSender -z $zabbixServer -c $zabbixAgentConfig -s "$baculaHost" -k "bacula.running.jobs.alert" -o "$running_jobs"
+    $zabbixSender -z $zabbixServer \
+        -c $zabbixAgentConfig \
+        -s "$baculaHost" \
+        -k "bacula.running.jobs.alert" \
+        -o "$running_jobs"
 fi
